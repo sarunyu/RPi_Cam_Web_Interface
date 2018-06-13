39854b9c7aa546be9490ca614ad6939c02ad899f
Sarunyu Chomphuphon <sarunyoo.cho@gmail.com>
2018-06-14 00:20:50 +0700

    Update SendLine.sh

--- www/macros/SendLine.sh
+++ www/macros/SendLine.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+
+line_token=
+
 echo "Sending Line Notify"
-curl -X POST -H "Authorization: Bearer WzZweyeasnB7JKPBbHZ11dOEytNlExPOGJiNjqouANk" -F "message=Motion Detect" -F "imageFile=@$1" https://notify-api.line.me/api/notify
+curl -X POST -H "Authorization: Bearer $line_token" -F "message=Motion Detect" -F "imageFile=@$1" https://notify-api.line.me/api/notify
 echo "Sending Email"
 /usr/bin/python3 /var/www/html/macros/SendEmail.py $1
